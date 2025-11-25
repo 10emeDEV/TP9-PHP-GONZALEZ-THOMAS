de86132345a603fc95092e16ba2ce7ed7061772a
10emeDEV <10emepro@gmail.com>
2025-11-25 11:11:24 +0100

    Contraignez l'usage de vos classes

--- Code/index.php
+++ Code/index.php
@@ -11,9 +11,41 @@
 
 declare(strict_types=1);
 
-class QueuingPlayer extends Player
+abstract class AbstractPlayer
 {
-    public function __construct(Player $player, protected int $range = 1)
+    public function __construct(protected string $name, protected float $ratio = 400.0)
+    {
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    protected function probabilityAgainst(self $player): float
+    {
+        return 1 / (1 + (10 ** (($player->getRatio() - $this->getRatio()) / 400)));
+    }
+
+    public function updateRatioAgainst(self $player, int $result): void
+    {
+        $this->ratio += 32 * ($result - $this->probabilityAgainst($player));
+    }
+
+    public function getRatio(): float
+    {
+        return $this->ratio;
+    }
+}
+
+/** @noinspection PhpUnused */
+final class Player extends AbstractPlayer
+{
+}
+
+final class QueuingPlayer extends AbstractPlayer
+{
+    public function __construct(AbstractPlayer $player, protected int $range = 1)
     {
         parent::__construct($player->getName(), $player->getRatio());
     }
@@ -24,7 +56,7 @@ class QueuingPlayer extends Player
     }
 }
 
-class Lobby
+final class Lobby
 {
     /** @var array<QueuingPlayer> */
     public array $queuingPlayers = [];
@@ -41,12 +73,13 @@ class Lobby
         });
     }
 
-    public function addPlayer(Player $player): void
+    public function addPlayer(AbstractPlayer $player): void
     {
         $this->queuingPlayers[] = new QueuingPlayer($player);
     }
 
-    public function addPlayers(Player ...$players): void
+    /** @noinspection PhpParamsInspection */
+    public function addPlayers(AbstractPlayer ...$players): void
     {
         foreach ($players as $player) {
             $this->addPlayer($player);
@@ -54,36 +87,12 @@ class Lobby
     }
 }
 
-class Player
-{
-    public function __construct(protected string $name, protected float $ratio = 400.0)
-    {
-    }
-
-    public function getName(): string
-    {
-        return $this->name;
-    }
-
-    private function probabilityAgainst(self $player): float
-    {
-        return 1 / (1 + (10 ** (($player->getRatio() - $this->getRatio()) / 400)));
-    }
-
-    public function updateRatioAgainst(self $player, int $result): void
-    {
-        $this->ratio += 32 * ($result - $this->probabilityAgainst($player));
-    }
-
-    public function getRatio(): float
-    {
-        return $this->ratio;
-    }
-}
-
+/** @var Player $greg */
 $greg = new Player('greg', 400);
+/** @var Player $jade */
 $jade = new Player('jade', 476);
 
+/** @noinspection PhpParamsInspection */
 $lobby = new Lobby();
 $lobby->addPlayers($greg, $jade);
 
